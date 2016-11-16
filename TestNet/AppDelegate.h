95cea2eafa9b65ffdd3d689174e46e811dbb94fc
StarJ <Starj@StarJdeMacBook-Pro.local>
2016-11-16 15:28:26 +0800

    Initial Commit

---
+++ TestNet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestNet
+//
+//  Created by StarJ on 2016/11/16.
+//  Copyright © 2016年 StarJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
